6ebcf98f428fe8022cb0ca44f57718b11db08ff8
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-20 13:53:48 -0400

    added player inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,8 +1,15 @@
+const Potion = require('../lib/Potion');
 const { expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 
+jest.mock('../lib/Potion');
+
 test('creates a player object', () => {
     const player = new Player('Dave');
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
