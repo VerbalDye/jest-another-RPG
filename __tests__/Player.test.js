bb7a1c1c3cceafb0a1800701d9ec20d697ee1a5c
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-20 15:06:26 -0400

    added player methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -15,4 +15,23 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
-})
+});
+
+test('get the players stats as an object', () => {
+    const player = new Player('Jack');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
